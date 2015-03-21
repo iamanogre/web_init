dcc5619e7831f6f9d9339b320f782f5f5115822b
iamanogre <gqhoang@berkeley.edu>
2015-03-20 23:09:29 -0700

    infinite loop on default.txt

--- test.c
+++ test.c
@@ -16,20 +16,44 @@ int main() {
 
 	FILE *fp;
 	char buf[1000];
+	char user_web_choices[10];
 	char *text_mode = "r";
-	char *text_name = "websites.txt";
+	char *text_default = "default.txt";
 	char *shell_command = "open";
 		/* SW_SHOWNORMAL = 1 */
 	int shell_mode = SW_SHOWNORMAL;
+	int user_input;
 
-	fp = fopen(text_name, text_mode);
+	/* open file  and error check */
+	fp = fopen(text_default, text_mode);
 	if (fp == NULL) {
 		printf("Failed to open file");
 		exit(1);
 	}
-	while(fgets(buf, 1000, fp)!= NULL) {
-		ShellExecute(NULL, shell_command, buf, NULL, NULL, shell_mode);
+
+	/* infinite while loop */
+	while(1) {
+		rewind(fp);
+
+		printf("Defaulting...Press 0 to continue\n");
+		
+		/* probably a better way to do this, but wait until user input is 0 */
+		while(1) {
+			scanf("%d", &user_input);
+			if (user_input == 0) {
+				memcpy(user_web_choices, text_default, sizeof(text_default));
+				break;
+			}
+		}	
+
+		/* open up all the default websites */
+		while(fgets(buf, 1000, fp)!= NULL) {
+			ShellExecute(NULL, shell_command, buf, NULL, NULL, shell_mode);
+		}
+		
 	}
+
 	fclose(fp);
+
 	return 0;
 }
